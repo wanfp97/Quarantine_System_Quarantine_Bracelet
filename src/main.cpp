6286dda9bcf87b842ccc678a8a5f19fc5c17e57f
wanfp97 <63029301+wanfp97@users.noreply.github.com>
2021-06-18 17:49:17 +0800

    debugged some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,13 @@ enum QUARANTINE_BAND_STATUS {
   NOT_INIT  = 0,
   PRESENT   = 1,
   OPENED    = 2,
-  MISSING   = 3,      //never used by qb (for ig only)
+  MISSING   = 3,
   EMERGENCY = 4
 };
 
-RF24 nrf24(8, 9); // using pin 7 for the CE pin, and pin 8 for the CSN pin
+RF24 nrf24(8, 9); // using pin 8 for the CE pin, and pin 9 for the CSN pin
 
-SoftwareSerial IG(7, 6);     //pin 5 Rx, pin4 Tx fot connecting to IG
+SoftwareSerial IG(6, 7);     //pin 6 Rx, pin 7 Tx for connecting to IG
 
 bool qb_initialized = 0;
 
@@ -35,15 +35,15 @@ struct settings{
 struct qb_message
 {
   uint8_t qb_num;
-  uint8_t status = 0;
-}qb_message;
+  uint8_t status;
+}qb_msg;
 
 
 void setup() {
   IG.begin(9600);
   while(!IG);   //wait until uart port ready
 
-  pinMode(2, INPUT);     //use pin2 for lock, externally connect to pull up resistor
+  pinMode(2, INPUT_PULLUP);     //use pin2 for lock
   pinMode(3, INPUT_PULLUP);     //use pin3 for emergency button
   pinMode(10, OUTPUT);    // used for buzzer
   digitalWrite(10, LOW);
@@ -86,9 +86,12 @@ void setup() {
    Serial.println("start sync");
   #endif
 
+  char* incoming_str = (char*)malloc(sizeof("connect"));
+  char expecting[] = "connect";
+
   Retry_sync:
   while(IG.available()<=0);
-  String incoming_str = IG.readString();
+  IG.readBytes(incoming_str, sizeof(expecting));
 
   #ifdef DEBUG
     delay(100);
@@ -96,28 +99,47 @@ void setup() {
     Serial.println(incoming_str);
   #endif
 
-  if(incoming_str == "connect") {
-    IG.print(F("connected"));
+  if(strncmp(incoming_str, expecting, sizeof(expecting))==0) {
+    char ack[] = "connected";
+    IG.write(ack, sizeof(ack));
 
     #ifdef DEBUG
-      Serial.println("connected sent");
+      Serial.println("Sent: connected");
     #endif
+
+    struct qb_sync_message_incoming {
+      uint8_t ig_channel;
+      uint32_t ig_addr;
+      uint32_t qb_addr;
+      uint8_t qb_num;
+    }qb_sync_info;
 
     while(IG.available()<=0);   //wait for serial input
-    incoming_str = IG.readString();
+    IG.readBytes(reinterpret_cast<char*>(&qb_sync_info), sizeof(qb_sync_info));
 
+    qb_settings.channel = qb_sync_info.ig_channel;
     #ifdef DEBUG
-      delay(100);
-      Serial.print(F("Received: "));
-      Serial.println(incoming_str);
+      Serial.print(F("qb_settings.channel = "));
+      Serial.println(qb_settings.channel);
     #endif
 
-    void* incoming_ptr = incoming_str.begin();
-    qb_settings.channel = *(uint16_t*) incoming_ptr;
-    qb_settings.ig_addr = *(unsigned int*) incoming_ptr + 2;    //offset to ig_addr (refer to message sent by ig)
-    qb_settings.qb_addr = *(unsigned int*) incoming_ptr + 6;    //offset to qb_addr
-    qb_message.qb_num = *(uint8_t*) incoming_ptr + 10;          //offset to qb_num
-    IG.print(incoming_str);
+    qb_settings.ig_addr =  qb_sync_info.ig_addr;   
+    #ifdef DEBUG
+      Serial.print(F("qb_settings.ig_addr = "));
+      Serial.println(qb_settings.ig_addr, 16);
+    #endif
+
+    qb_settings.qb_addr =  qb_sync_info.qb_addr;
+    #ifdef DEBUG
+      Serial.print(F("qb_settings.qb_addr = "));
+      Serial.println(qb_settings.qb_addr, 16);
+    #endif
+
+    qb_msg.qb_num =  qb_sync_info.qb_num;
+    #ifdef DEBUG
+      Serial.print(F("qb_msg.qb_num = "));
+      Serial.println(qb_msg.qb_num);
+    #endif
 
     nrf24.setPALevel(RF24_PA_MAX);      //set max signal amplification
     nrf24.setCRCLength(RF24_CRC_8);     //set 8 bit crc
@@ -129,57 +151,89 @@ void setup() {
     nrf24.openWritingPipe(qb_settings.ig_addr);         //set to write to ig
     nrf24.stopListening();     //start receiving
 
-    delay(100);
+    IG.write(reinterpret_cast<char*>(&qb_sync_info), sizeof(qb_sync_info));
 
     #ifdef DEBUG
-      delay(100);
-      Serial.print(F("nrf24 parameter setting done"));
+      Serial.println(F("nrf24 parameter setting done"));
     #endif
 
-    qb_message.status = PRESENT;
+    if(digitalRead(2)){
+      qb_msg.status = PRESENT;
+    }
+    else {
+      qb_msg.status = OPENED;
+    }
+    
+    delay(1000);   //delay for IG to be ready
 
-    bool report = nrf24.write(&qb_message, sizeof(qb_message));      // transmit & save the report
+    #ifdef DEBUG
+      Serial.println(F("transmitting to IG"));
+    #endif
+
+    bool report = nrf24.write(&qb_msg, sizeof(qb_message));      // transmit & save the report
 
     if(!report) {
-      goto Retry_sync;
       #ifdef DEBUG
         delay(100);
-        Serial.print(F("transmission failed"));
+        Serial.println(F("transmission failed"));
+        Serial.println(F("goto Retry sync"));
       #endif
+      goto Retry_sync;
+    }
+    else {
+      #ifdef DEBUG
+        delay(100);
+        Serial.println(F("sync successful"));
+      #endif  
     }
   }
   else {
     goto Retry_sync;
   }
   
+  
 
   //sync successful
 
   Retry_test_update:
   while(IG.available()<=0);
-  incoming_str = IG.readString();
-  if(incoming_str == "update") {
+  IG.readBytes(incoming_str, sizeof("update")-1);
+  if(strncmp(incoming_str,"update", sizeof("update")-1)==0) {
+    if(digitalRead(2)){
+      qb_msg.status = PRESENT;
+    }
+    else {
+      qb_msg.status = OPENED;
+    }
     IG.print(F("start"));
-    if(!nrf24.write(&qb_message, sizeof(qb_message))) {            // transmit & save the report
+    #ifdef DEBUG
+      Serial.println(F("Sending update message"));
+    #endif
+    if(!nrf24.write(&qb_msg, sizeof(qb_message))) {            // transmit & save the report
+      #ifdef DEBUG
+        Serial.println(F("Sending message failed"));
+      #endif
       goto Retry_test_update;
     }
   }
   else {
     goto Retry_test_update;
   }
+  #ifdef DEBUG
+    Serial.println(F("Update message sent"));
+  #endif
 }
 
 void loop() {
   qb_transmit();
   nrf24.powerDown();    
-  delay(50);        //delay for the power down nrf24 to complete
   for(int i = 0; i<37; i++) {     //loop to sleep for 5mins
     LowPower.powerDown(SLEEP_8S, ADC_ON, BOD_OFF);        //sleep for 8s and leave ADC as default (disabled previously)
   }
 }
 
 void qb_opened() {
-  qb_message.status = OPENED;
+  qb_msg.status = OPENED;
   digitalWrite(10, HIGH);
   delay(2000);
   digitalWrite(10, LOW);
@@ -187,7 +241,7 @@ void qb_opened() {
 }
 
 void qb_emergency() {
-  qb_message.status = EMERGENCY;
+  qb_msg.status = EMERGENCY;
   digitalWrite(10, HIGH);
   delay(5000);
   digitalWrite(10, LOW);
@@ -196,10 +250,18 @@ void qb_emergency() {
 
 void qb_transmit() {
   Retransmit:
+  uint8_t retransmit_count = 0;
   nrf24.powerUp();
-  if(!nrf24.write(&qb_message, sizeof(qb_message))) {
+  if(!nrf24.write(&qb_msg, sizeof(qb_message))) {
     nrf24.powerDown();
     digitalWrite(10, HIGH);
+    retransmit_count ++;
+    if(retransmit_count>10) {
+      qb_msg.status = MISSING;
+      #ifdef DEBUG
+        Serial.println(F("Update info failed"));
+      #endif
+    }
     delay(100);
     digitalWrite(10, LOW);
     for(int j = 0; j<7; j++) {     //loop to sleep for 1min
@@ -207,6 +269,9 @@ void qb_transmit() {
     }
     goto Retransmit;
   }
+  #ifdef DEBUG
+    Serial.println(F("Update info success"));
+  #endif
   nrf24.powerDown();
 }
 
